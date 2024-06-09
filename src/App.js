6718eed0ec63482087627213c98f917823950c1f
shankar <skaligi007@gmail.com>
2024-06-09 09:49:34 +0530

    preview card completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-
+import MainPic from './assets/illustration-article.svg'
+import ProfilePic from './assets/image-avatar.webp'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className='container'>
+    <div className='card'>
+      <div className='image'>
+        <img src={MainPic} alt='main pic' width={336} height={200}/>
+      </div>
+      <div className='description'>
+        <div className='heading'>Learning</div>
+        <div className='published'>Published 21 Dec 2023</div>
+        <h1 className='title'>HTML & CSS Foundations</h1>
+        <div className='desc'>These languages are the backbone of every website defining structure, content and presentation.</div>
+      </div>
+      <div className='profile'>
+        <img src={ProfilePic} alt='profilepic' width={32} height={32}/>
+        <h3>Greg Hooper</h3>
+      </div>
     </div>
+   </div>
   );
 }
 
